5d037259612f5d768c8ce30ea6c284d2fe3ea8f0
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-09 14:14:29 -0300

    configure azure function in dotnet isolated mode

---
+++ src/Blob.Storage.Listner/Function1.cs
@@ -0,0 +1,21 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Blob.Storage.Listner
+{
+    public class Function1
+    {
+        private readonly ILogger _logger;
+
+        public Function1(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Function1>();
+        }
+
+        [Function("Function1")]
+        public void Run([BlobTrigger("samples-workitems/{name}", Connection = "")] string myBlob, string name)
+        {
+            _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {myBlob}");
+        }
+    }
+}
