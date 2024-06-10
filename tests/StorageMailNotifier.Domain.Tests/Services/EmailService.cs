6b62d23a5706ed83a8476689efa058c27652d7af
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-10 16:06:05 -0300

    Send_Email_With_From_Information (RED)

---
+++ tests/StorageMailNotifier.Domain.Tests/Services/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using StorageMailNotifier.Domain.Contracts.Services;
+using StorageMailNotifier.Domain.Models;
+
+namespace StorageMailNotifier.Domain.Tests.Services;
+
+internal class EmailService : IEmailService
+{
+    public Task NotifyAsync(NotifyEmailRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/StorageMailNotifier.Domain.Tests/Services/EmailServiceShould.cs
@@ -0,0 +1,43 @@
+﻿using NSubstitute;
+using StorageMailNotifier.Domain.Contracts.Repositories;
+using StorageMailNotifier.Domain.Contracts.Services;
+using StorageMailNotifier.Domain.Models;
+using System.Net.Mail;
+
+namespace StorageMailNotifier.Domain.Tests.Services;
+
+internal class EmailServiceShould
+{
+    [Test]
+    public async Task Send_Email_With_From_Information()
+    {
+        #region Arrange(Given)
+
+        var request = new NotifyEmailRequest();
+
+        var emailRepositoryMock = Substitute.For<IEmailRepository>();
+
+        IEmailService emailService = new EmailService();
+
+        string from = "raffaeleloi.lab@gmail.com";
+
+        #endregion
+
+        #region Act(When)
+
+        await emailService.NotifyAsync(request);
+
+        #endregion
+
+        #region Assert(Then)
+
+        await emailRepositoryMock
+            .Received()
+            .SendEmailAsync(Arg.Is<MailMessage>(
+                mailMessage => 
+                    mailMessage.From!.Address == from)
+            );
+
+        #endregion
+    }
+}
