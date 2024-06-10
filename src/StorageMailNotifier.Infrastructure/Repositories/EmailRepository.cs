42ecd388ac72f9dfa4762b06a90f58f3e8b1efaf
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-10 16:34:00 -0300

    refactor EmailRepository

--- src/StorageMailNotifier.Infrastructure/Repositories/EmailRepository.cs
+++ src/StorageMailNotifier.Infrastructure/Repositories/EmailRepository.cs
@@ -8,10 +8,35 @@ public class EmailRepository : IEmailRepository
 {
     public async Task SendEmailAsync(MailMessage mail)
     {
-        var smtpClient = new SmtpClient(host: "smtp.gmail.com", port: 587);
+        SmtpClient smtpClient = CreateSmtpClient();
 
+        AddCredentials(smtpClient);
+
+        EnableSSL(smtpClient);
+
+        await SendEmail(smtpClient, mail);
+    }
+
+    private static SmtpClient CreateSmtpClient()
+    {
+        return new SmtpClient(
+            host: "smtp.gmail.com", 
+            port: 587
+        );
+    }
+
+    private static void AddCredentials(SmtpClient smtpClient)
+    {
         smtpClient.Credentials = new NetworkCredential("your-email@example.com", "your-email-password");
+    }
+
+    private static void EnableSSL(SmtpClient smtpClient)
+    {
         smtpClient.EnableSsl = true;
+    }
+
+    private static async Task SendEmail(SmtpClient smtpClient, MailMessage mail)
+    {
         await smtpClient.SendMailAsync(mail);
     }
 }
