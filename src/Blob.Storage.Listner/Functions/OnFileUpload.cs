0b265f2c84cdd03ebc8310fcfd9abfd288d29d3d
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-10 16:47:34 -0300

    Notify_When_Upload (RED)

--- src/Blob.Storage.Listner/Functions/OnFileUpload.cs
+++ src/Blob.Storage.Listner/Functions/OnFileUpload.cs
@@ -5,7 +5,7 @@ namespace Blob.Storage.Listner.Functions;
 public class OnFileUpload
 { 
     [Function("OnFileUpload")]
-    public void Run([BlobTrigger("samples-workitems/{fileName}", Connection = "")] string content, string fileName, string blobTrigger, Uri uri)
+    public async Task Run([BlobTrigger("samples-workitems/{fileName}", Connection = "")] string content, string fileName, string blobTrigger, Uri uri)
     {
             var x = "";
     }
---
+++ tests/Blob.Storage.Listner.Tests/Functions/OnFileUploadShould.cs
@@ -0,0 +1,47 @@
+﻿using Blob.Storage.Listner.Functions;
+using NSubstitute;
+using StorageMailNotifier.Application.Contracts.UseCases;
+using StorageMailNotifier.Application.Models;
+
+namespace Blob.Storage.Listner.Tests.Functions;
+
+internal class OnFileUploadShould
+{
+    [Test]
+    public async Task Notify_When_Upload()
+    {
+        #region Arrange(Given)
+
+        string content = "My content";
+        string fileName = "my-file.txt";
+        string blobTrigger = "mycontainer/my-file.txt";
+        string url = "https://azure/storageaccount/mycontainer/my-file.txt";
+        var uri = new Uri(url);
+
+        var onFileUpload = new OnFileUpload();
+
+        var notifyFileUploadedMock = Substitute.For<INotifyFileUploaded>();
+
+        #endregion
+
+        #region Act(When)
+
+        await onFileUpload.Run(content, fileName, blobTrigger, uri);
+
+        #endregion
+
+        #region Assert(Then)
+
+        await notifyFileUploadedMock
+            .Received()
+            .NotifyAsync(Arg.Is<OnFileUploadFinished>(
+                request => 
+                    request.BlobContent == content &&
+                    request.FileName == fileName &&
+                    request.BlobTrigger == blobTrigger &&
+                    request.Uri == uri
+            ));
+
+        #endregion
+    }
+}
